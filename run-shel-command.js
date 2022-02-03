43385fb46d4fa6fcc68eadbadc5b013b800c8e27
Vaibhav Shanker <vaibhav.shanker@digitaljalebi.com>
2022-02-03 17:13:10 +0530

    n

--- run-shel-command.js
+++ run-shel-command.js
@@ -7,9 +7,14 @@ const dest_repo_url = "https://github.com/vaibhav-digitaljalebi/action-learn-1.g
 const det_repo_branch = "master"
 
 
-exec(`git remote add carbon-copy ${dest_repo_url}`, dump);
-exec(`git pull ${current_repo} ${current_branch} && git push carbon-copy ${det_repo_branch}`, dump);
 exec(`git remote remove carbon-copy`, dump);
+exec(`git remote add carbon-copy ${dest_repo_url}
+    && 
+    git pull ${current_repo} ${current_branch} && git push carbon-copy ${det_repo_branch}
+    &&
+    git remote remove carbon-copy`, dump);
+// exec(`git pull ${current_repo} ${current_branch} && git push carbon-copy ${det_repo_branch}`, dump);
+// exec(`git remote remove carbon-copy`, dump);
 
 function dump(err, stdout, stderr) {
     if(err) console.error("err", err);
